3cd2b8826860c24391d25e32d705559f7da3f93d
Suneel Manyam <sunilhari9@gmail.com>
2015-05-12 08:08:49 +0530

    Mergin issue resolved

--- custom/js/items.js
+++ custom/js/items.js
@@ -1,14 +1,10 @@
 $.getJSON( "custom/js/products.json", function( items ) {
-<<<<<<< HEAD
+
 	var displayItemDOM = function(ProductName){
 		var item = '<div class="media"><div class="media-left"><a href="#"><img class="img media-object" src="./custom/images/food/1.jpg" alt="Test"></a></div><div class="media-body media-top"><h4 class="media-heading">'+ProductName+'<div class="pull-right more">...</div></h4><div class="summary">Sample TestSample TestSample TestSample TestSample TestSample TestSample TestSample TestSample TestSample TestSample TestSample TestSample Test</div></div><div class="media-right"><span class="glyphicon glyphicon-minus" title="Click to decrease"></span>                              <input type="text" value=1 class="quantity"><span class="glyphicon glyphicon-plus" title="Click to increase"></span></span></div>';
-=======
 sessionStorage.setItem('products',JSON.stringify(items));
 
-	var displayItemDOM = function(ProductName,ProductCode){
-		var item = '<div class="media"><div class="media-left"><a href="#"><img class="img media-object" src="./custom/images/food/1.jpg" alt="Test"></a></div><div class="media-body media-top"><h4 class="media-heading">'+ProductName+'<div class="pull-right more">...</div></h4><div class="summary">Sample TestSample TestSample TestSample TestSample TestSample TestSample TestSample TestSample TestSample TestSample TestSample TestSample Test</div></div><div class="media-right"><span class="glyphicon glyphicon-minus" title="Click to decrease"></span><span class="quantity" data-product-code="'+ProductCode+'">1</span><span class="glyphicon glyphicon-plus" title="Click to increase"></span><span class="glyphicon glyphicon-edit"></span></div>';
->>>>>>> origin/master
-		$('.items').append(item);
+	$('.items').append(item);
 	};
 	var getUniqueCategories = function(){ 
 		var categories = [];
