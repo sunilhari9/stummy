aa7b12d506418f070abea3ce77eb40767006d3f0
Suneel Manyam <sunilhari9@gmail.com>
2015-06-12 10:15:17 +0530

    Checkout pages updated with local storage values

--- custom/js/checkout.js
+++ custom/js/checkout.js
@@ -2,16 +2,39 @@
     
     var navListItems = $('ul.setup-panel li a'),
         allWells = $('.setup-content');
-
     allWells.hide();
+    if(localStorage.getItem("homeAddress")==null){
+    var homeAddress={};
+        homeAddress.Name= "Suneel";
+        homeAddress.Email= "sunilhari9@gmail.com";
+        homeAddress.Phone= "9705984789";
+        homeAddress.Address= "Hyd";
+        localStorage.setItem("homeAddress", JSON.stringify(homeAddress));
+}
+if(localStorage.getItem("deliveryAddress")==null){
+     var deliveryAddress={};
+     deliveryAddress.Name= "Suneel";
+        deliveryAddress.Email= "sunilhari9@gmail.com";
+        deliveryAddress.Phone= "9705984789";
+        deliveryAddress.Address= "Hyd";
+        localStorage.setItem("deliveryAddress", JSON.stringify(deliveryAddress));
+}
     var homeAddressLocalStorage = JSON.parse(localStorage.getItem("homeAddress"));
-    $("#displayHomeName").text(homeAddressLocalStorage.Name)
-    $("#displayHomeAddress").text(homeAddressLocalStorage.Address + ", Ph:" + homeAddressLocalStorage.Phone ) 
+   $("#displayHomeName").text(homeAddressLocalStorage.Name)
+   $("#displayHomeAddress").text(homeAddressLocalStorage.Address + ", Ph:" + homeAddressLocalStorage.Phone );
+      $("#homeName").val(homeAddressLocalStorage.Name);
+     $("#homeEmail").val(homeAddressLocalStorage.Email);
+     $("#homePhone").val(homeAddressLocalStorage.Phone);
+     $("#homeAddr").val(homeAddressLocalStorage.Address);
+      
     
     var deliveryAddressLocalStorage = JSON.parse(localStorage.getItem("deliveryAddress"));
-      console.log(deliveryAddressLocalStorage)
-    $("#displayDelivaryName").text(deliveryAddressLocalStorage.Name)
-    $("#displayDelivaryAddress").text(deliveryAddressLocalStorage.Address + ", Ph:" + deliveryAddressLocalStorage.Phone )
+      $("#displayDelivaryName").text(deliveryAddressLocalStorage.Name)
+        $("#displayDelivaryAddress").text(deliveryAddressLocalStorage.Address + ", Ph:" + deliveryAddressLocalStorage.Phone )
+        $("#deliveryName").val(deliveryAddressLocalStorage.Name);
+        $("#deliveryEmail").val(deliveryAddressLocalStorage.Email);
+        $("#deliveryPhone").val(deliveryAddressLocalStorage.Phone);
+        $("#deliveryAddr").val(deliveryAddressLocalStorage.Address);
       $("#homeAddressSubmit").click(function(){
          var homeAddress={};
         homeAddress.Name= $("#homeName").val();
@@ -76,7 +99,7 @@ $("#deliveryAddressSubmit").click(function(){
 				for(var index = 0;index<cartTotalItemsLength;index++){
 					var eachItemData = cartTotalItems.itemsArray[index];
 					 var productSubTotal= eachItemData.product_qty * 25;
-                var CheckOutItems='<tr><td data-th=""><div class="row"><div class="col-sm-2 hidden-xs"><img src="http://placehold.it/100x100" alt="..." class="img-responsive"/></div><div class="col-sm-6"><h4 class="nomargin productName">'+eachItemData.product_name+'</h4></div>          </div></td><td data-th="Price" class="price">&#8377; 25 </td><td data-th="Quantity">'+eachItemData.product_qty+'</td><td data-th="Subtotal" class="text-center" class="subtotal">&#8377;'+productSubTotal+' </td>  <td class="actions" data-th=""><button class="btn btn-danger btn-sm"><i class="glyphicon glyphicon-trash deleteItem"></i></button></td>     </tr>'
+                var CheckOutItems='<tr><td data-th=""><div class="row"><div class="col-sm-2 hidden-xs"><img src="http://placehold.it/100x100" alt="..." class="img-responsive"/></div><div class="col-sm-6"><h4 class="nomargin productName">'+eachItemData.product_name+'</h4></div>          </div></td><td data-th="Price" class="price">&#8377; 25 </td><td data-th="Quantity">'+eachItemData.product_qty+'</td><td data-th="Subtotal" class="text-center" class="subtotal">&#8377;'+productSubTotal+' </td>  <td class="actions" data-th=""></td>     </tr>'
                     
 					cartItemDescHtml += CheckOutItems;
 				}
